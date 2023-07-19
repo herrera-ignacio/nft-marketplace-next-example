eb1e8737b4b743c2235909b06f83a12e3e78925b
Ignacio Herrera <ignacioromanherrera@gmail.com>
2023-07-18 23:50:36 -0300

    Ethers and Metamask connection

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,9 +1,40 @@
-import { FunctionComponent, createContext, useContext, useState } from "react";
+import { FunctionComponent, createContext, useContext, useEffect, useState } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Contract, BrowserProvider } from "ethers";
+import { browser } from "process";
 
-const Web3Context = createContext<any>(null);
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
+
+export type Web3Params = {
+  ethereum?: MetaMaskInpageProvider;
+  provider?: BrowserProvider;
+  contract?: Contract;
+}
+
+export type Web3State = {
+  isLoading: boolean;
+} & Web3Params;
+
+const Web3Context = createContext<Web3State>(createDefaultState());
 
 const Web3Provider: FunctionComponent<{ children: any }> = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({ test: "Hello Provider" });
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+  useEffect(() => {
+    setWeb3Api({
+      ethereum: window.ethereum, // Injected by metamask.
+      provider: new BrowserProvider(window.ethereum),
+      isLoading: false
+    });
+  }, []);
+
+  if (web3Api.provider) {
+    web3Api.provider?.send("eth_requestAccounts", []);
+  }
 
   return (
     <Web3Context.Provider value={web3Api}>
@@ -12,8 +43,15 @@ const Web3Provider: FunctionComponent<{ children: any }> = ({ children }) => {
   );
 };
 
-export function useWeb3() {
-  return useContext(Web3Context);
-}
+export const useWeb3 = () => useContext(Web3Context);
+
+function createDefaultState() {
+  return {
+    ethereum: undefined,
+    provider: undefined,
+    contract: undefined,
+    isLoading: true
+  };
+};
 
 export default Web3Provider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,23 @@ import NftList from "@/components/nft/list";
 import nfts from "../content/meta.json";
 import { NftMeta } from "@/types/nft";
 import { useWeb3 } from "@/components/providers/web3";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  const { test } = useWeb3();
+  const { ethereum, provider, isLoading, contract } = useWeb3();
+  // console.log("Ethereum", ethereum);
 
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await provider?.listAccounts();
+      console.log(accounts);
+    };
+
+    getAccounts();
+  }, [provider]);
+  
   return (
     <BaseLayout>
-    {test}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
